2d82989cdd13fb4a1f9d98e325b20d3a126f471c
Marco Vasko-Klima <marcoaklima@gmail.com>
2026-02-11 16:50:56 -0500

    Cleaned Up Comments and Unnecessary Files

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,14 +8,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants
 {
     public static class OperatorConstants
@@ -61,6 +53,7 @@ public final class Constants
         public static final double kRlOffset = Math.PI;
         public static final double kRrOffset = Math.PI / 2;
 
+        // SparkMax and SparkFlex CAN ID's
         public static final int kFlDriveId = 1;
         public static final int kFrDriveId = 2;
         public static final int kRrDriveId = 3;
